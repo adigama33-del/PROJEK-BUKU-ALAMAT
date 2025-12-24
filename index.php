cf1668d9d1a02365b3e4a6b7880262c048bd4800
Wisnu Adigama <adigama33@gmail.com>
2025-12-24 14:10:47 +0800

    3. Membuat halaman index (header dan form pencarian kontak)

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="Title">
+            <h1>Buku Alamat</h1>
+            <p>Kelola Kontak pribadi Anda</p>
+        </div>
+        <button><a href="tambah_kontak.php">Tambah Kontak</a></button>
+    </header>
+    <section>
+        <form action="" method="post" class="search-bar">
+            <input type="text" name="search" placeholder="Cari kontak...">
+            <button type="submit">Cari</button>
+        </form>
+    </section>
+</body>
+</html>
